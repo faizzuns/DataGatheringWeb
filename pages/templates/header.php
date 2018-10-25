0564b39cd9f7d0c52f81c464d9683c1ec1a12d75
faizzuns <faiz.sahupala29@gmail.com>
2018-10-25 22:09:18 +0700

    make home page

--- pages/templates/header.php
+++ pages/templates/header.php
@@ -1,15 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-<body>
-=======
     <meta charset="UTF-8">
     <title>Data Gathering</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/app.css">
 </head>
 <body>
->>>>>>> affd407b688208ccff02a23c8e811ce0023d8a5f
