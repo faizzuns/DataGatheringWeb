52b2562f89d09abadf9748eb7d248718fcfd601d
faizzuns <faiz.sahupala29@gmail.com>
2018-10-26 20:41:04 +0700

    add redir.php

--- pages/templates/header.php
+++ pages/templates/header.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,7 +8,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/app.css">
-
 
 </head>
 
