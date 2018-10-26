13747a00dd0e2345eb1dcc1ffaa6e40bc7e59005
Yasya <yasya.rusyda@gmail.com>
2018-10-26 17:03:57 +0700

    Add 2 more pages

---
+++ pages/Distract.php
@@ -0,0 +1,24 @@
+<?php
+require 'templates/header.php';
+?>
+
+    <div class="bg">
+    </div>
+
+    <div class="backdrop"></div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <div style="height: 70px;"></div>
+                <div class="box-home padding-medium">
+                    <div class="margin-top-medium flex center-horizontal text-size-medium"><strong>ikan hiu makan hiu, yuk main yuk</strong></div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+require 'templates/footer.php';
+?>
