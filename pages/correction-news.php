90e9f52ff50a91bf48fe73100a3f88eaddcb1dfe
faizzuns <faiz.sahupala29@gmail.com>
2018-11-03 15:49:39 +0700

    add correction news content

--- pages/correction-news.php
+++ pages/correction-news.php
@@ -5,6 +5,15 @@ if ($row['tendency'] == null) {
     die();
 }
 require 'templates/header.php';
+
+if ($_GET['category'] == '2') {
+    $content = '
+    <p>Dilansir dari kompas.com pada tanggal 19 Januari 2018, berita dengan judul Jokowi: Pemerintah targetkan 3000 Kapal untuk Nelayan Lokal di tahun 2018 telah ditarik. Kompas.com menyatakan bahwa ternyata berita yang dipulbikasi sehari yang lalu adalah berita yang tidak valid kebenerannya. Selain itu, kompas.com melakukan ralat terhadap informasi tersebut yakni program 3000 Kapal untuk Nelayan sebenarnya bukanlah program yang dicanangkan oleh pemerintah. Melainkan, salah satu program Corporate Social Responsibility (CSR) dari salah satu perusahaan swasta yang bergerak di bidang tambang mineral. Oleh karena itu, tim redaksi dari kompas.com melakukan permohonan maaf karena sudah menyebarkan informasi yang tidak dapat dipercaya.</p>
+    ';
+} else {
+    $content = '<p>Dilansir dari kompas.com pada tanggal 28 Agustus 2018, berita dengan judul Infrastruktur buat siapa? Jokowi menandatangani revisi PP Dana  Desa, 25% Dana Desa Dialokasikan untuk Pembangun Infrastruktur Nasional telah tarik dari situs kompas.com. Hal tersebut dilakukan oleh pihak redaksi kompas.com karena berita tersebut memuat informasi-informasi yang tidak valid dan tidak dapat dipercaya. Kompas.com menyatakan bahwa berita yang baru dipublikasi sehari yang lalu merupakan berita yang tidak valid kebenarannya. Selain itu, kompas.com meralat bahwa Jokowi tidak menetapkan 25% dana desa tidak dialokasikan untuk infrastruktur nasional. Melainkan, 25% dana desa dimaksudkan untuk pembangunan infrastruktur desa itu sendiri agar mempermudah mobilitas kegiatan ekonomi masyarakat desa. Hal tersebut dilakukan pemerintah dalam rangka pemerataan pembangunan di desa maupun di kota. Oleh karena itu, tim redaksi dari kompas.com melakukan permohonan maaf karena sudah menyebarkan informasi yang tidak dapat dipercaya.</p>';
+}
+
 ?>
 
     <div class="bg">
@@ -20,7 +29,8 @@ require 'templates/header.php';
                     <div class="margin-top-medium flex center-horizontal text-size-medium"><strong>Ralat Berita</strong></div>
                     <div class="flex center-horizontal text-size-very-small" style="text-align: justify">
                         <div class="margin-large">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                            <?php echo $content;?>
+                        </div>
                     </div>
                     <form class="flex center-horizontal margin-bot-medium" action="Distract.php">
                         <input type="submit" value="Lanjut" class="btn btn-outline-dark quarter">
