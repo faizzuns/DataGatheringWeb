8917a79af49b6f27b5be5e12da298e11f392d7ca
faizzuns <faiz.sahupala29@gmail.com>
2018-11-06 15:43:59 +0700

    revert

--- php/tendency-action.php
+++ php/tendency-action.php
@@ -7,18 +7,15 @@
  * Time: 9:41 PM
  */
 
-header("Location: http://ahmadfaiz.us/gather/pages/home.php");
-die();
-
-//require 'connect.php';
-//$sql = "UPDATE ahmadfai_gather.`transaction` SET ahmadfai_gather.`transaction`.tendency = " . $_POST['tendency'] . " WHERE ahmadfai_gather.`transaction`.id = " . $_COOKIE['trans'];
-//if ($conn->query($sql) === TRUE) {
-//    setcookie('tendency', $_POST['tendency'], time()+3600, '/');
-//    header('Location: ../pages/news.php');
-//    die();
-//} else {
-//    echo "Error: " . $sql . "<br>" . $conn->error;
-//}
-//$conn->close();
+require 'connect.php';
+$sql = "UPDATE ahmadfai_gather.`transaction` SET ahmadfai_gather.`transaction`.tendency = " . $_POST['tendency'] . " WHERE ahmadfai_gather.`transaction`.id = " . $_COOKIE['trans'];
+if ($conn->query($sql) === TRUE) {
+    setcookie('tendency', $_POST['tendency'], time()+3600, '/');
+    header('Location: ../pages/news.php');
+    die();
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
 
 ?>
