9103b6bfdaef9d112f47a4bb3461f04e44d9809a
faizzuns <faiz.sahupala29@gmail.com>
2018-11-06 15:34:33 +0700

    test 2

--- php/tendency-action.php
+++ php/tendency-action.php
@@ -7,15 +7,18 @@
  * Time: 9:41 PM
  */
 
-require 'connect.php';
-$sql = "UPDATE ahmadfai_gather.`transaction` SET ahmadfai_gather.`transaction`.tendency = " . $_POST['tendency'] . " WHERE ahmadfai_gather.`transaction`.id = " . $_COOKIE['trans'];
-if ($conn->query($sql) === TRUE) {
+header("Location: ../pages/news.php");
+die();
+
+//require 'connect.php';
+//$sql = "UPDATE ahmadfai_gather.`transaction` SET ahmadfai_gather.`transaction`.tendency = " . $_POST['tendency'] . " WHERE ahmadfai_gather.`transaction`.id = " . $_COOKIE['trans'];
+//if ($conn->query($sql) === TRUE) {
 //    setcookie('tendency', $_POST['tendency'], time()+3600, '/');
-    header('Location: ../pages/news.php');
-    die();
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();
+//    header('Location: ../pages/news.php');
+//    die();
+//} else {
+//    echo "Error: " . $sql . "<br>" . $conn->error;
+//}
+//$conn->close();
 
 ?>
